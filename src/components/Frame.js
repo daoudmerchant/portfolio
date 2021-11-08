5e0ab70241f66caf09c5a31131d22a1ac2f68968
Daoud Merchant <merchantdaoud@hotmail.com>
2021-11-08 11:02:49 +0100

    Add basic responsive banner

--- src/components/Frame.js
+++ src/components/Frame.js
@@ -1,20 +1,41 @@
 import tw from "tailwind-styled-components/dist/tailwind";
 
-const Top = tw.div`
-    h-20
-    bg-black
-    w-9/10
-    transform
-    skew-x-minus45
+const Panel = tw.div`
     absolute
-    top-0
-    right-10
+    bg-black
+    transform
 `;
 
-const Frame = () => {
+const Horizontal = tw(Panel)`
+h-60px
+left-30px
+right-30px
+md:h-80px
+md:left-40px
+md:right-40px
+skew-x-minus45
+${(props) => (props.side === "TOP" ? "top-0" : "bottom-0")}
+`;
+
+const Vertical = tw(Panel)`
+  w-60px
+  top-30px
+  bottom-30px
+  md:w-80px
+  md:top-40px
+  md:bottom-40px
+  skew-y-minus45
+  ${(props) => (props.side === "RIGHT" ? "right-0" : "left-0")}
+`;
+
+const Frame = ({ corner }) => {
+  /*
+        corner = "TOPRIGHT" || "BOTTOMLEFT"
+    */
   return (
     <>
-      <Top />
+      <Horizontal side="TOP" />
+      <Vertical side="RIGHT" />
     </>
   );
 };
